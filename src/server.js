d312b5a761ca30e8d6b4e75a572ed3028f1080da
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-12-16 18:49:09 +0700

    Hapi Plugin & Joi Data Validation

--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
+  const notesService = new NotesService();
 
-    server.route(routes);
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    await server.start();
-    console.log(`The server is running on ${server.info.uri}`);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`The server is running on ${server.info.uri}`);
 };
 
 init();
